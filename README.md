fe886b3e36b2baa43c593abc6ce69c94e837c14b
shalri <fsaonlinework777@gmail.com>
2024-04-06 17:38:54 +0800

    Home page and single page

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
+## Links
+
+- [Todo](/home/shalri/notes/tutorials/nextblog-todo.norg)
+
 ## Getting Started
 
 First, run the development server:
---
+++ app/layout.jsx
@@ -0,0 +1,34 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Link from "next/link";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "The Bubbly Baker",
+  description: "My amazing recipe app",
+};
+
+export default function RootLayout({ children }) {
+  let header = (
+    <header>
+      <Link href="/">
+        <h1>The Bubbly Baker</h1>
+      </Link>
+    </header>
+  );
+  let footer = (
+    <footer>
+      FS<strong>&#123;</strong>&#32;&#32;code&#32;&#32;<strong>&#125;</strong>
+    </footer>
+  );
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {header}
+        {children}
+        {footer}
+      </body>
+    </html>
+  );
+}
